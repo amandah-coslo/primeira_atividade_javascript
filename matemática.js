df1bcd0d4efd4c08261de373a7dc2b2cdd4620f3
amandah-coslo <amandahcostal@hotmail.com>
2022-03-31 17:02:09 -0300

    Finalizado

--- matemática.js
+++ matemática.js
@@ -1,50 +1,31 @@
-var resposta = Number(prompt('Escolha uma das opções: 1- Operações básicas, 2- porcentagem, 3- Média Aritmédica e 4- Média Ponderada'));
+var resposta = Number(prompt('Escolha uma das opções: 1- operações básicas (soma, sub, div e mult), 2- porcentagem, 3- Média Aritmédica, 4- Média Ponderada e 5- sair'));
 
 
-{
-    var soma = function(n1, n2){
-        var soma = n1 + n2;
-        return soma;
-    }
-
-    var sub = function(n1, n2){
-        var sub = n1 - n2;
-        return sub;
-    }
-
-    var div = function(n1, n2){
-        var div = n1 / n2;
-        return div;
-    }
-
-    var mult = function(n1, n2){
-        var mult = n1 * n2;
-        return mult;
-    }
-
-    var calculadora = function(n1, n2, operacao){
-        resultado = operacao(n1, n2);
-        return resultado;
+if (resposta == 1) {
+    var operacoesBasicas = function(n1, n2){
+        if (opera === "soma") {
+            var soma = n1 + n2;
+            return console.log('O valor da',opera,'é:',soma);
+        } else if (opera === "sub"){
+            var sub = n1 - n2;
+            return console.log('O valor da',opera,'é:',sub);;
+        } else if (opera === "div") {
+            var div = n1 / n2;
+            return console.log('O valor da',opera,'é:',div);;
+        } else if (opera === "mult") {
+            var mult = n1 * n2;
+            return console.log('O valor da',opera,'é:',mult);
+        }
     }
 
     var n1 = Number(prompt('Número:'));
     var n2 = Number(prompt('Número:'));
     var opera = prompt('operacao: "soma, sub, div ou mult": ');
 
-    if (opera === "soma") {
-        var res = calculadora(n1, n2, soma);
-    } else if (opera === "sub") {
-        var res = calculadora(n1, n2, sub);
-    } else if (opera === "div") {
-        var res = calculadora(n1, n2, div);
-    } else {
-        var res = calculadora(n1, n2, mult);
+    if (opera === "soma" || "sub" || "div" || "mult") {
+        operacoesBasicas(n1,n2);
     }
-
-    console.log('O valor da',opera,'é:',res);
-}
-
-{
+} else if (resposta == 2) {
     var percent = Number(prompt('Porcentagem: '));
     var num = Number(prompt('Número: '));
     
@@ -56,44 +37,49 @@ var resposta = Number(prompt('Escolha uma das opções: 1- Operações básicas,
     var resultado_p = porcentagem(percent, num)
 
     console.log(percent + '%' + ' de ' + num + ' = ' + resultado_p);  
-}   
-
-{
-    var cont = 0;
-    var soma = 0;
-    var quantNotas = Number(prompt('Quantidades de notas desejadas: '));
-    for(i = quantNotas; i > 0; i--) {
-        var num = Number(prompt('Digite a nota'));
-        soma += num;
-        cont += 1;
-    }
-
-    var totalNotas = soma / quantNotas;
-
-    console.log('A média Aritmética das notas digitadas é:',totalNotas)
-}
-/*falta terminar*/
-{
-    var notas=[];
-    var pesos=[];
-
-    var contNotas = 0;
-    var contPesos = 0;
-    var quantNotas = Number(prompt('Digite a quantidade de notas : '));
+} else if (resposta == 3) {
+    var num = [];
+        var contnum = 0;
+        function Lista () {
+            var quantNotas = Number(prompt('Quantidades de notas desejadas: '));
+            for(i = 0; i < quantNotas; i++) {
+                num.push(Number(prompt('Digite a nota')));
+                contnum += num[i];
+            } 
+        resultado = contnum / num.length;
     
-    for(i = quantNotas; i > 0; i--){
-        notas = Number(prompt('Digite a nota desejada: '));
-        pesos = Number(prompt('Digite o peso da nota: '));
+        return console.log(resultado);
+
+        }
+        Lista();
+
+} else if (resposta == 4) {
+    var notas = [];
+    var pesos = [];
+    
+    var contPesos = 0;
+
+    var totalNotasPesos = 0;
+
+    var quantNotas = Number(prompt('Digite a quantidade de notas : '));
+    for(i = 0; i < quantNotas; i++){
+        notas.push(Number(prompt('Digite a nota desejada: ')));
+        pesos.push(Number(prompt('Digite o peso da nota:')));
         
-        contNotas += 1;
-        contPesos += 1;
-        
+        contPesos += pesos[i];
+
         if(contPesos >= 11){
             console.log('Não é possivel calcular a media');
-        
         }
+
+        totalNotasPesos += (notas[i] * pesos[i]);
     }
-    
-    var mPonderada =(notas[i] * pesos[i])/(contPesos);
+
+    var mPonderada = totalNotasPesos/10;
+
     console.log('Sua nota é: ' + mPonderada);
+} else if (resposta == 5) {
+    console.log("Saindo do programa...");
+} else{
+    console.log("Opção inválida");
 }
